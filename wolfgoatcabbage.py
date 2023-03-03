954bcaa0f3750237ac2b5aa9836ec552eb139c86
Jogata5 <jogata@csu.fullerton.edu>
2023-03-02 22:12:49 -0800

    Updates

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -9,20 +9,23 @@ class Node:
 
 
 class WolfGoatCabbage:
-    def __init__(self, initial, goal=[{},{'F', 'W', 'G', 'C'}]):
+    def __init__(self, initial, goal=({},{'F', 'W', 'G', 'C'})):
         self.initial = initial
         self.goal = goal
-        
     
     def get_left(self, state):
         return state.index(0)
     
     def get_right(self, state):
-        return state.index(0
-)
+        return state.index(1)
+    
     def actions(self, state):
         possible_actions = [{'F'}, {'F', 'C'}, {'F', 'W'}, {'F', 'G'}]
         return possible_actions
+    
+    def goal_test(self, state):
+        return state == self.goal
+    
         
         
         
